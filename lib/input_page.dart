d59ededbf1c20beae8474270e373337013cc7229
Sukitha <sudugamasooriya@palo-it.com>
2021-06-24 10:52:33 +0800

    Code clean up and first page

---
+++ lib/input_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+    );
+  }
+}
+//test
